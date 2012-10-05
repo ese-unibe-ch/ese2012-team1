61d1f59a761bdf65a27b089c96db6732c8eaa515
Salim Hermidas <s.hermidas@gmail.com>
2012-10-05 14:49:59 +0200

    user saves himself into classvariable hash users on creation.

--- trade/app/init.rb
+++ trade/app/init.rb
@@ -9,7 +9,6 @@ require_relative('../../trade/app/models/user')
 include Models
 
 userA = Models::User.created( "userA", "passwordA" )
-userA.save
 aa = userA.create_item("UserA_ItemA", 10)
 ab = userA.create_item("UserA_ItemB", 50)
 ab.to_active
@@ -17,7 +16,6 @@ ac = userA.create_item("UserA_ItemC", 120)
 ac.to_active
 
 userB = Models::User.created( "userB", "passwordB" )
-userB.save
 ba = userB.create_item("UserB_ItemA", 10)
 ba.to_active
 bb = userB.create_item("UserB_ItemB", 50)
@@ -25,7 +23,6 @@ bb.to_active
 bc = userB.create_item("UserB_ItemC", 120)
 
 userC = Models::User.created( "userC", "passwordC" )
-userC.save
 ca = userC.create_item("UserC_ItemA", 10)
 ca.to_active
 cb = userC.create_item("UserC_ItemB", 50)
@@ -33,4 +30,3 @@ cc = userC.create_item("UserC_ItemC", 120)
 cc.to_active
 
 ese = Models::User.created( "ese", "ese" )
-ese.save
