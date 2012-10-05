f9e8ad86392ce7452f92d24b9e18569595fefd9d
Pas <pascal@offenes-lernen.ch>
2012-10-05 13:20:54 +0200

    Finished passwordchecker.js
    Checks now password to have at least 6 characters and at least one
    small letter, one capitalletter and one digit.
    
    Display the progression in a label under input field for password.
    
    modified:   trade/app/public/java-script/passwordchecker.js

--- trade/app/public/java-script/passwordchecker.js
+++ trade/app/public/java-script/passwordchecker.js
@@ -19,10 +19,26 @@ function inputCheck(){
     var eingabe = document.getElementById("password").value;
     var messageField = document.getElementById("message");
 
-    if(eingabe == ""){
-        messageField.innerHTML = "Weak password";
+    if (eingabe.match(/[^a-zA-Z1-9]/)) {
+        messageField.innerHTML = "No special characters are allowed"
+    } else if(eingabe.length < 6) {
+        setTextAndColor(messageField, "Weak password. Too short", "red");
+    } else if (eingabe.match(/^[A-Z]+$/)) {
+        setTextAndColor(messageField, "Weak password. Only capitalized letters", "red");
+    } else if (eingabe.match(/^[a-z]+$/)) {
+        setTextAndColor(messageField, "Weak password. Only small letters", "red");
+    } else if (eingabe.match(/^\d+$/)) {
+        setTextAndColor(messageField, "Weak password. Only digits", "red");
+    } else if (eingabe.match(/^([a-zA-Z])+$/) || eingabe.match(/^([a-z]|\d)+$/)
+                    || eingabe.match(/^([A-Z]|\d)+$/) )  {
+        setTextAndColor(messageField, "Normal Password", "orange");
     } else {
-        messageField.innerHTML = "This is a really strong password!"
+        setTextAndColor(messageField, "Gracious password! You are now allowed to enter", "green");
     }
+}
+
+function setTextAndColor(messageField, text, color) {
+    messageField.innerHTML = text;
+    messageField.style.color = color;
 }
 
