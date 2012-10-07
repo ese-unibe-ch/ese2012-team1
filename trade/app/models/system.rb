bdaa84ef23945beccb854df4ae08b081db2441ef
lyriael <lyriael@gmx.net>
2012-10-07 17:59:32 +0200

    More in System
    Still not a lot, but I started with some basic methods. Currently the tests don't
    work, so there surly is a mistake (having troubles with hashs... -.-)
    
    Our Trade App still runs completely without the System.rb class, I will integrate it
    later, when I'm sure that it'll work. That means the classes user.rb and item.rb remain
    untouched.
    
    I probably wont' do more today, but I'm still on it. Since its not a Story I think it has no
    priority.
    
    	modified:   trade/app/models/system.rb
    	modified:   trade/test/system_test.rb

--- trade/app/models/system.rb
+++ trade/app/models/system.rb
@@ -5,5 +5,32 @@ module Models
   class System
     include Singleton
 
+    attr_accessor :users, :items, :item_id_count
+
+    def initialize
+      self.users ={}
+      self.items ={}
+      self.item_id_count = 0;
+    end
+
+    def add_user(user)
+      email = user.email
+      users = {email => user}
+      #TODO does not work as I intend...
+    end
+
+    def add_item(item)
+      items = {:item_id_count => item}
+      item.id = item_id_count + 1
+    end
+
+    def remove_user_by_id(user_id)
+
+    end
+
+    def remove_user(user)
+
+    end
+
   end
 end
--- trade/test/system_test.rb
+++ trade/test/system_test.rb
@@ -2,13 +2,34 @@ require 'test/unit'
 require 'rubygems'
 require 'require_relative'
 require_relative('../app/models/system')
-
+require_relative('../app/models/user')
+require_relative('../app/models/item')
 class SystemTest < Test::Unit::TestCase
 
-  def test_item_initialisation
+  @momo
+  @beppo
+
+  def setup
+    @momo = Models::User.created("Momo",    "zeit",     "momo@mail.ch",   "Denn Zeit ist Leben",  "C:/bild.gif")
+    @beppo = Models::User.created("Beppo",  "strasse",  "beppo@gmail.ch", "Schritt für Schritt", "C:/bild.gif")
+  end
+
+  def teardown
+    @momo.clear
+    @beppo.clear
+  end
+
+  def test_singleton_initialisation
     system_a = Models::System.instance
     system_b = Models::System.instance
     assert(system_a == system_b, "Two instances should be the same")
+  end
+
+  def test_should_add_user
+    system = Models::System.instance
+    system.add_user(@momo)
+    system.add_user(@beppo)
+    assert(system.users.size == 2, "there should be 2 users, but there were #{system.users.size} user(s).")
 
   end
 
