4fbd48dc4894dad94bd974814e437f73d90703fe
Pas <pascal@offenes-lernen.ch>
2012-10-08 12:18:39 +0200

    Refactoring StringChecker method is_email?
    Does now make the check in 3 different stages.
    
    1) Check if there is a @
    2) Check if local part looks good
    3) Check if domain part looks good
    
    modified:   trade/app/helpers/string_checkers.rb

--- trade/app/helpers/string_checkers.rb
+++ trade/app/helpers/string_checkers.rb
@@ -13,6 +13,16 @@ class String
   ##
 
   def is_email?
-    self =~ /[A-Za-z123456789._-]+@[A-Z.a-z123456789-]+\.[a-z]+$/
+    return false unless (self.include?('@'))
+    parts = (self.split('@', 2))
+    local_part = parts[0]
+    domain_part = parts[1]
+    return false if (domain_part.include?('@'))
+
+    eval_local_part = local_part =~ /[A-Za-z123456789._-]+/
+    eval_domain_part = domain_part =~ /[A-Z.a-z123456789-]+\.[a-z]+$/
+
+    eval_domain_part && eval_local_part
   end
+
 end
