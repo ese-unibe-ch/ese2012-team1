15e214af1af9aa42246867c1767e980fb19bbb23
Pas <pascal@offenes-lernen.ch>
2012-10-08 17:30:02 +0200

    Button to register is active if password is strong
    And if the retyped password is the same. Had to change
    register.haml to give the button an id.
    
    modified:   trade/app/public/java-script/passwordchecker.js
    modified:   trade/app/views/register.haml

--- trade/app/public/java-script/passwordchecker.js
+++ trade/app/public/java-script/passwordchecker.js
@@ -3,6 +3,9 @@
  * eingabefelder-waehrend-der-eingabe-mit-javascript-zu-ueberpruefen
  */
 
+var strong = false; /*Should be true if password in password field is strong*/
+var same_password = false; /*Should be true if both passwords are the same*/
+
 /**
  * Schreibt den Event onkeyup in das Input-Feld.
  */
@@ -23,21 +26,30 @@ function inputCheck(){
     var messageField = document.getElementById("message");
 
     if (eingabe.match(/[^a-zA-Z1-9]/)) {
-        messageField.innerHTML = "No special characters are allowed"
+        messageField.innerHTML = "No special characters are allowed";
+        strong = false
     } else if(eingabe.length < 6) {
         setTextAndColor(messageField, "Weak password. Too short", "red");
+        strong = false
     } else if (eingabe.match(/^[A-Z]+$/)) {
         setTextAndColor(messageField, "Weak password. Only capitalized letters", "red");
+        strong = false
     } else if (eingabe.match(/^[a-z]+$/)) {
         setTextAndColor(messageField, "Weak password. Only small letters", "red");
+        strong = false
     } else if (eingabe.match(/^\d+$/)) {
         setTextAndColor(messageField, "Weak password. Only digits", "red");
+        strong = false
     } else if (eingabe.match(/^([a-zA-Z])+$/) || eingabe.match(/^([a-z]|\d)+$/)
         || eingabe.match(/^([A-Z]|\d)+$/) )  {
         setTextAndColor(messageField, "Password must have lowercase, uppercase and digits", "red");
+        strong = false
     } else {
         setTextAndColor(messageField, "Gracious password! You are now allowed to enter", "green");
+        strong = true;
     }
+
+    setButtonState()
 }
 
 function re_inputCheck(){
@@ -47,9 +59,13 @@ function re_inputCheck(){
 
     if (eingabe == re_eingabe) {
         setTextAndColor(re_messageField, "OK, both fields ar identically.", "green");
+        same_password = true
     } else {
         setTextAndColor(re_messageField, "Please retype your password.", "red");
+        same_password = false
     }
+
+    setButtonState()
 }
 
 function setTextAndColor(messageField, text, color) {
@@ -57,3 +73,16 @@ function setTextAndColor(messageField, text, color) {
     messageField.style.color = color;
 }
 
+/**
+ * Sets button state depending on the variables strong and same_password
+ */
+
+function setButtonState() {
+    var button = document.getElementById('register').disabled;
+    if (strong == true && same_password == true) {
+        button.disabled = false;
+    }
+    else {
+        button.disabled = true;
+    }
+}
