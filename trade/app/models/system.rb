00b95e012f77565f5ade17db1b3f661755fd22a2
lyriael <lyriael@gmx.net>
2012-10-11 22:39:09 +0200

    System provides basic methods for user
    	modified:   trade/app/models/system.rb
    Added short class doc.
    
    The following methods are avaiable, but not tested yet:
    - add_user(user)
    - fetch_user(user_email) --> returns user object
    - fetch_all_users_but(user_email) --> returns array of all other users
    - remove_user(user_email)

--- trade/app/models/system.rb
+++ trade/app/models/system.rb
@@ -1,34 +1,54 @@
 require 'singleton'
 
 module Models
-
+  # This class serves as some kind of database. It holds all organisations (identified by name),
+  # all users (identified by email) and all items (identified by id).
+  # It is implemented as a Singleton.
   class System
     include Singleton
 
-    attr_accessor :users, :items, :item_id_count
+    attr_accessor :organisation, :users, :items, :item_id_count
 
     def initialize
-      self.users ={}
-      self.items ={}
+      self.organisation = {}
+      self.users = {}
+      self.items = {}
       self.item_id_count = 0
     end
 
+
+    # Adds an user to the system.
     def add_user(user)
+      fail "No user" if (user == nil)
       self.users.store(user.email, user)
     end
 
+    # Returns the user with associated email.
+    def fetch_user(user_email)
+      fail "No user with email #{user_email}" if self.users.member?(user_email)
+      self.users.fetch(user_email)
+    end
+
+    # Removes an user from the system.
+    def remove_user(user_email)
+      fail "No user with email #{user_email}" if self.users.member?(user_email)
+      self.users.delete(user_email)
+    end
+
+    def fetch_all_users_but(user_email)
+      fail "No user with email #{user_email}" if self.users.member?(user_email)
+      self.users.values - [user_email]  # Array difference
+    end
+
+
+
+    # Adds an item to the system and increments the id counter for items.
     def add_item(item)
+      fail"An item must have an owner when added to the system." if (item.owner == nil)
       items = {:item_id_count => item}
       item.id = item_id_count + 1
     end
 
-    def remove_user_by_id(user_id)
-
-    end
-
-    def remove_user(user)
-
-    end
 
   end
 end
