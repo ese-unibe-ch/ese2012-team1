2689be955e8b9db611a89f8d3d98cca6ec6f2725
Salim Hermidas <s.hermidas@gmail.com>
2012-10-11 15:25:05 +0200

    item image change v5

--- trade/app/controllers/creator.rb
+++ trade/app/controllers/creator.rb
@@ -94,9 +94,9 @@ module Controllers
         filename = params[:item_picture][:filename]
         #file_path ="#{dir}#{id}.#{filename.sub(/.*\./, "")  }"
         #File.unlink(file_path)
-        file_path ="#{dir}#{new_item.get_id}#{File.extname(filename)}"
+        file_path ="#{dir}#{id}#{File.extname(filename)}"
         File.copy(tempfile.path, file_path)
-        file_path = "../images/users/#{id}.#{filename.sub(/.*\./, "")}"
+        file_path = "../images/items/#{id}.#{filename.sub(/.*\./, "")}"
         Item.get_item(id).add_picture(file_path)
       end
 
