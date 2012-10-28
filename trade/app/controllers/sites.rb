89e515f3362da8b4b571a14435a6c51350cc6b16
Salim Hermidas <s.hermidas@gmail.com>
2012-10-28 14:46:31 +0100

    error page: if error code not correct, redirect /home

--- trade/app/controllers/sites.rb
+++ trade/app/controllers/sites.rb
@@ -75,30 +75,24 @@ module Controllers
         msg = ""
         if params[:title] == "Not_A_Number"
           msg = "Price should be a number!"
-        end
-        if params[:title] == "Not_Enough_Credits"
+        elsif params[:title] == "Not_Enough_Credits"
           msg = "Sorry, but you can't buy this item, because you have not enough credits!"
-        end
-        if params[:title] == "No_Valid_Account_Id"
+        elsif params[:title] == "No_Valid_Account_Id"
           msg = "Your account id could not be found"
-        end
-        if params[:title] == "No_Valid_User"
+        elsif params[:title] == "No_Valid_User"
           msg = "Your email could not be found"
-        end
-        if params[:title] == "No_Name"
+        elsif params[:title] == "No_Name"
           msg = "Please enter a name"
-        end
-        if params[:title] == "No_Description"
+        elsif params[:title] == "No_Description"
           msg = "Please enter a description"
-        end
-        if params[:title] == "No_Valid_Item_Id"
+        elsif params[:title] == "No_Valid_Item_Id"
           msg = "The requested item id could not be found"
-        end
-        if params[:title] == "Choose_Another_Name"
+        elsif params[:title] == "Choose_Another_Name"
           msg = "The name you chose is already taken, choose another one"
-        end
-        if params[:title] == "No_Self_Remove"
+        elsif params[:title] == "No_Self_Remove"
           msg = "You can not remove yourself from your organisation"
+        else
+          redirect '/home'
         end
         haml :error, :locals => {:error_title => params[:title], :error_message => msg}
     end
