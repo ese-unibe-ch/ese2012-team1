16411a1a72869a6e3b37b9ca9e4f7dc9e7e94514
Pas <pascal@offenes-lernen.ch>
2012-11-26 09:36:49 +0100

    Documentation for SearchItem
    modified:   trade/app/models/search/search_item.rb

--- trade/app/models/search/search_item.rb
+++ trade/app/models/search/search_item.rb
@@ -1,12 +1,34 @@
 #Abstract Class
 
 module Models
-  class SearchItem
-    attr_accessor :item, :symbol_methods, :name, :method_priority, :user_priority
 
-    def initialize
-      self.method_priority = 1
-    end
+  ##
+  #
+  #  Responsibility:
+  #  Holding the methods to be called when performing
+  #  a Search (see Search). Provides the priorities of
+  #  of the user performing a search.
+  #
+  ##
+
+  class SearchItem
+    attr_accessor :item, :symbol_methods, :name, :user_priority
+
+    ##
+    #
+    # Creates a SearchItem holding an item (normally User
+    # Organisation or Item), a name and methods to be called.
+    # The given methods should be provided as symbols and
+    # should return an Object that can respond to #include?
+    # When performing a search the items a group by the
+    # given name (see Search and SearchResult).
+    #
+    # Params:
+    # item: item to be called by symbol_methods
+    # name: Name by which item should be grouped
+    # symbol_methods: Array of methods (provided as symbols)
+    #
+    ##
 
     def self.create(item, name, symbol_methods)
       search_item = self.new
@@ -17,6 +39,13 @@ module Models
       search_item
     end
 
+    ##
+    #
+    # Returns 1 it the user with the given id is part
+    # part of the item stored. Returns 2 if not.
+    #
+    ##
+
     def priority_of_user(user_id)
       self.part_of?(user_id) ? 1 : 2
     end
@@ -24,7 +53,8 @@ module Models
     ##
     #
     # Checks if the user with the given user_id is part
-    # of the SearchItem
+    # of the SearchItem. Return true if this is the
+    # case.
     #
     ##
 
