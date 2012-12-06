48a0fadaf4bf81fb7e715ca36e3a23c0b210c23d
Pas <pascal@offenes-lernen.ch>
2012-12-06 16:07:35 +0100

    Removed before in Controller::Authentication
    Added before method in each method instead in before.
    
    modified:   trade/app/controllers/authentication.rb

--- trade/app/controllers/authentication.rb
+++ trade/app/controllers/authentication.rb
@@ -1,25 +1,13 @@
-require 'rubygems'
-require 'require_relative'
-require 'sinatra/base'
-require 'haml'
-require 'sinatra/content_for'
-require_relative '../models/user'
-require_relative '../helpers/alert'
-require_relative('../helpers/render')
-require_relative '../helpers/before'
-
 include Models
 include Helpers
 
 module Controllers
   class Authentication < Sinatra::Application
-    before do
-      before_for_user_not_authenticated
-    end
-
     set :views , "#{absolute_path('../views', __FILE__)}"
 
     get '/login' do
+      before_for_user_not_authenticated
+
       redirect '/home' if session[:auth]
 
       session[:navigation].select(:unregistered)
@@ -28,11 +16,15 @@ module Controllers
     end
 
     get '/logout' do
+      before_for_user_not_authenticated
+
       redirect "/" unless session[:auth]
       haml :'authentication/logout'
     end
 
     post "/authenticate" do
+      before_for_user_not_authenticated
+
       if (!Models::System.instance.user_exists?(params[:username]))
         session[:alert] = Alert.create("", "No such user or password", true)
         redirect '/login'
@@ -56,6 +48,8 @@ module Controllers
     end
 
     post "/unauthenticate" do
+      before_for_user_not_authenticated
+
       session[:user] = nil
       session[:auth] = false
       session.clear
