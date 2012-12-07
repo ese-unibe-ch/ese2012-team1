d7f7e4dfc558e115c501ee817abb2f43e36eeaf0
Pas <pascal@offenes-lernen.ch>
2012-12-07 22:59:05 +0100

    Documenation for Conversation
    modified:   trade/app/models/Messenger/conversation.rb

--- trade/app/models/Messenger/conversation.rb
+++ trade/app/models/Messenger/conversation.rb
@@ -1,11 +1,26 @@
 require 'observer'
 
+##
+#
+# A conversation holds all messages belonging to one subject
+# Each conversation has a unique id.
+# A conversation knows all its participants
+# A conversation can be observed. If a new message is put
+# to it then all observers are notified. The conversation
+# itself and the message_id are then returned.
+# An observer has to implement the method
+# #update(conversation, message_id)
+#
+##
+
 class Conversation
   include Observable
   @@conversations_count = 1
 
-  attr_reader :conversation_id, :messages
-  attr_accessor :subscribers
+  attr_reader :conversation_id, #Unique id to address the Conversation
+              :messages         #Messages belonging to this subject
+
+  attr_accessor :subscribers    #Participants
 
   def initialize
     @conversation_id = @@conversations_count
@@ -15,10 +30,14 @@ class Conversation
     @subscribers = Array.new
   end
 
+  ##
+  #
+  # Creates a Conversation and storing its subscribers.
   #
   # Params:
   # subscribers : all ids of users that are subscribed to this conversation
   #
+  ##
 
   def self.create(subscribers)
     fail "should have subscribers" if (subscribers.nil?)
@@ -35,6 +54,14 @@ class Conversation
     users_id.all? { |user_id| System.instance.account_exist?(user_id) }
   end
 
+  ##
+  #
+  # Adds a new message to this conversation
+  # If a message has reply_to then this message is stored
+  # right behind the message it replies to.
+  #
+  ##
+
   def add_message(message)
     fail "Message needs id" unless message.respond_to?(:id)
     fail "Message should respond to reply_to" unless message.respond_to?(:reply_to)
@@ -44,6 +71,12 @@ class Conversation
     changed
     notify_observers self, message.id
   end
+
+  ##
+  #
+  # Count of all messages belonging to this Conversation
+  #
+  ##
 
   def count_messages
     @messages.size
