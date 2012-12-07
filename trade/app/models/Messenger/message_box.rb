e2f3d66c6f4257ddc6449c14607c340d98d1ae2d
Salim Hermidas <s.hermidas@gmail.com>
2012-12-07 19:04:03 +0100

    Created Classes: MessageBox Messenger
    Prepared Tests:
    MessageBoxTest
    MessengerTest

--- trade/app/models/Messenger/message_box.rb
+++ trade/app/models/Messenger/message_box.rb
@@ -1,3 +1,77 @@
-class MessageBox
-  # To change this template use File | Settings | File Templates.
+require 'conversation'
+require 'message'
+
+module Models
+  # This class implements the MessageBox for Users.
+  # It holds all conversations (identified by id).
+  #
+  # Responsibility:
+  # Controlling of Message Reading (mark as Read)
+  # Observes all Conversations that are in the MessageBox
+
+  class MessageBox
+    attr_accessor :conversations, :owner, :message_tree
+
+    ##
+    #
+    # Create a new MessageBox for User with ID user_id.
+    # Params: user_id : Integer (User ID)
+    #
+    ##
+    def create(user_id)
+      self.conversations = Hash.new
+      self.owner = user_id
+      self.message_tree = Hash.new
+    end
+
+    ##
+    #
+    # Add Conversation to Users MessageBox.
+    # Params: conversation : Conversation
+    #
+    ##
+    def add_conversation(conversation)
+
+    end
+
+    ##
+    #
+    # Check if Message was already read by User.
+    # Params: conv_id : Integer (Conversation ID)
+    #         mess_id : Integer (Message ID)
+    #
+    ##
+    def read?(conf_id, mess_id)
+
+    end
+
+    ##
+    #
+    # Counting number of new Messages.
+    #
+    ##
+    def new_messages_count
+
+    end
+
+    ##
+    #
+    # Check if there are unread Messages.
+    #
+    ##
+    def new_messages?
+      self.new_messages_count == 0 ? false : true
+    end
+
+    ##
+    #
+    # Counting number of all Messages.
+    #
+    ##
+    def messages_count
+
+    end
+
+  end
+
 end
--- trade/app/models/Messenger/messenger.rb
+++ trade/app/models/Messenger/messenger.rb
@@ -1,3 +1,86 @@
-class Messenger
-  # To change this template use File | Settings | File Templates.
+require 'singleton'
+require 'message_box'
+
+module Models
+  # This class is the global messenger.
+  # It holds all MessageBoxes (identified by user_id).
+  # It is implemented as a Singleton.
+  #
+  # Responsibility:
+  # Control of all MessageBoxes
+  # Control of sending and receiving messages
+
+  class Messenger
+    include Singleton
+
+    attr_accessor :message_boxes
+
+    def initialize
+      self.message_boxes = Hash.new
+    end
+
+    ##
+    #
+    # Create a new Conversation between two or more users.
+    # Params: from    : Integer (User ID)
+    #         to      : Array[Integer] (User IDs)
+    #         subject : String
+    #         message : String
+    #  Add Conversation to subscribers MessageBox
+    #
+    ##
+    def new_message(from, to, subject, message)
+
+    end
+
+    ##
+    #
+    # Answer to a Message in a Conversation to all (ore some) users in a conversation.
+    # Params: from    : Integer (User ID)
+    #         to      : Array[Integer] (User IDs)
+    #         subject : String
+    #         message : Message
+    #         conv_id : Integer
+    #         mess_id : Integer
+    #  Add message to conversation
+    #
+    ##
+    def answer_message(from, to, subject, message, conv_id, mess_id)
+
+    end
+
+    ##
+    #
+    # Get MessageBox of user.
+    # Params: user_id : Integer (User ID)
+    #
+    ##
+    def get_message_box(user_id)
+
+    end
+
+
+    ##
+    #
+    # Create MessageBox for User with ID user_id.
+    # Params: user_id : Integer (User ID)
+    #
+    ##
+    def register(user_id)
+
+    end
+
+
+    ##
+    #
+    # Delete MessageBox for User with ID user_id.
+    # Params: user_id : Integer (User ID)
+    #
+    ##
+    def unregister(user_id)
+
+    end
+
+  end
+
 end
