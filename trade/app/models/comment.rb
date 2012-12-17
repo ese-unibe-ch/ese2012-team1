c0bb81312eda685d83ec39cff02c3aecfe90c603
Pas <pascal@offenes-lernen.ch>
2012-12-17 19:00:53 +0100

    A more rdoc convenient documentation for Comment
    modified:   comment.rb

--- trade/app/models/comment.rb
+++ trade/app/models/comment.rb
@@ -1,25 +1,50 @@
 module Models
+  ##
+  #
+  #  A Comment is written by a user. It holds the text, the
+  #  subject, the date and its creator.
+  #
+  #  Comment is a CommentContainer.
+  #  With this inheritance it is possible
+  #  to make a comment on a comment.
+  #
+  ##
+
   class Comment < CommentContainer
-    ##
-    #
-    #  A Comment is a CommentContainer.
-    #  With this inheritance it is possible
-    #  to make a comment on a comment.
-    #
-    ##
-    attr_accessor :comment, :creator, :header, :nr, :date_stamp
+
+    # Content itself (String)
+    attr_accessor :comment
+    # Writer of the comment (Account)
+    attr_accessor :creator
+    # Title of the comment (String)
+    attr_accessor :header
+    # Unique number of the comment (Integer)
+    attr_reader :nr
+    # Date when the comment was written (Time)
+    attr_accessor :date_stamp
 
     @@unique_nr = 0
+
+    def initialize
+      @nr = @@unique_nr
+      @@unique_nr += 1
+    end
 
     ##
     #
     # Creates a comment by saving his creator
-    # and the comment itself
+    # the comment, the creation time, his unique
+    # number and subject.
     #
-    # Expects:
-    # creator : the user who created this comment
-    # header : the title
-    # comment_text : the actual message
+    # Date and unique number are provided by
+    # this method while the other informations
+    # have to be passed as parameters.
+    #
+    # === Parameters
+    #
+    # +creator+:: account who created this comment
+    # +header+:: title
+    # +comment_text+:: the actual message
     #
     ##
     def self.create(creator, header, comment_text)
@@ -30,15 +55,12 @@ module Models
       comment.creator = creator
       comment.date_stamp = Time.now
 
-      comment.nr = @@unique_nr
-      @@unique_nr += 1
-
       comment
     end
 
     ##
     #
-    # Collects itself and all children comments.
+    # Returns an array including itself and all children comments.
     #
     ##
 
@@ -53,7 +75,7 @@ module Models
 
     ##
     #
-    # The string representation of a comment
+    # String representation of a comment
     #
     ##
     def to_s
