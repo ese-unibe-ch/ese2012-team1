49628d09cdad5809949f9e7dfebe59a0f4a49791
Pas <pascal@offenes-lernen.ch>
2012-12-18 17:30:50 +0100

    Made Message rdoc friendly
    modified:   Messenger/message.rb

--- trade/app/models/Messenger/message.rb
+++ trade/app/models/Messenger/message.rb
@@ -1,9 +1,29 @@
+##
+#
+# A Message stores a single message
+#
+##
+
 class Message
   @@message_count = 1
 
+  #unique id of the message
   attr_reader :message_id
 
-  attr_accessor :message, :sender, :date, :subject, :reply_to, :depth, :receivers
+  #message itself
+  attr_accessor :message
+  #sender of the message
+  attr_accessor :sender
+  #date when the message was created
+  attr_accessor :date
+  #subject of the message
+  attr_accessor :subject
+  #message id of the message this message replies to
+  attr_accessor :reply_to
+  #depth of the message indicating how deep this message is in the message tree
+  attr_accessor :depth
+  #receivers of this message
+  attr_accessor :receivers
 
   def initialize
     @message_id = @@message_count
@@ -16,12 +36,14 @@ class Message
   #
   # Creates a new message
   #
-  #Params: from : sender
-  #        to : receiver (Array)
-  #        subject : subject of the message
-  #        date : date of the message (Time)
-  #        message : the message itself
-  #        reply_to : id of the message this message replies to
+  # ==== Parameters
+  #
+  # +from+:: sender (can't be nil)
+  # +to+:: receiver as Array
+  # +subject+:: subject of the message (can't be nil)
+  # +date+:: date of the message as Time (can't be nil)
+  # +message+:: the message itself (can't be nil and can't be an empty string)
+  # +reply_to+:: id of the message this message replies to (must be nil or positive integers)
   #
   ##
 
@@ -49,7 +71,12 @@ class Message
   # Checks if a given user is the receiver
   # of this message.
   #
-  # Params: user_id : the id of the user that shall be checked
+  # Returns true if the user is a receiver
+  # false otherwise.
+  #
+  # === Parameters
+  #
+  # +user_id+:: id of the user that shall be checked
   #
   ##
 
@@ -62,7 +89,11 @@ class Message
   # Checks if a given user is the sender
   # of this message.
   #
-  # Params: user_id : the id of the user that shall be checked
+  # Returns true if the user is the sender
+  #
+  # === Parameters
+  #
+  # +user_id+:: id of the user to be checked
   #
   ##
 
@@ -70,5 +101,4 @@ class Message
     user_id.to_s == self.sender.to_s ? out = true : out = false
     return out
   end
-
 end
