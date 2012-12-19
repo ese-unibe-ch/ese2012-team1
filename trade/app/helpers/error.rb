c3ffa291162abc662a04666d65e646e9be8e4c27
Lafunamor <info@zipfuzapfu.ch>
2012-12-19 09:35:06 +0100

    enabled userfriendly errormessages
    removed unnecessary files

--- trade/app/helpers/error.rb
+++ trade/app/helpers/error.rb
@@ -1,6 +1,6 @@
 module ErrorHandling
   #To get userfriendly error messages set this to false
-  set :development, true
+  set :development, false
 
   error do
     session[:alert] = Alert.create("Ooops!", "Something went wrong...", true) unless (session[:alert])
